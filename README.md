e01ae413cc925ad5c3ce460f27515b4e299d711b
WoBKloBSquad <vaniersel66@gmail.com>
2016-04-03 16:08:40 +0200

    Created the Java Server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Restaurant-Billing-System
-###An open-source restaurant billing system. Created in Java, C#, PHP, HTML5, CSS3 and MySQL.
+An open-source restaurant billing system. Created in Java, C#, PHP, HTML5, CSS3 and MySQL.
---
+++ [JAVA] Server/src/com/github/wobklobsquad/restaurantbillingsystem/javaserver/Main.java
@@ -0,0 +1,13 @@
+package com.github.wobklobsquad.restaurantbillingsystem.javaserver;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+
+	
+	
+}
