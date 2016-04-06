f849b7f10ecc45fd200e5018b2ecdcd9b0aa3493
WoBKloBSquad <vaniersel66@gmail.com>
2016-04-06 18:21:12 +0200

    Updates

---
+++ [JAVA] Server/src/com/github/wobklobsquad/restaurantbillingsystem/javaserver/Commands.java
@@ -0,0 +1,7 @@
+package com.github.wobklobsquad.restaurantbillingsystem.javaserver;
+
+public class Commands {
+
+	public static final String add = "add";
+	public static final String remove = "remove";
+}
